a1c18ec322a02ae0efe2619186807fe30376c785
nikolaygelemerov <nikolaygelemerov@gmail.com>
2021-02-18 18:30:50 +0200

    test useCallback with prop no dep update

--- src/pages/AwaitSetStateFuncCmp/AwaitSetState.js
+++ src/pages/AwaitSetStateFuncCmp/AwaitSetState.js
@@ -1,76 +1,21 @@
-import React, { memo, useCallback, useLayoutEffect, useState } from 'react';
+import { memo, useState } from 'react';
 
-import { console, useUpdate } from '../../services';
+import AwaitSetStateChild from './AwaitSetStateChild';
 
 const AwaitSetState = () => {
-  const [countOne, setCountOne] = useState(0);
-  const [countTwo, setCountTwo] = useState(0);
-
-  const update = useCallback(async () => {
-    console({
-      value: `Func countOne before: ${countOne}`,
-      color: 'black',
-      bgColor: 'lightBlue'
-    });
-
-    // await setState (none documented behaviour).
-    await setCountOne(prevCount => prevCount + 1);
-
-    console({
-      value: `Func countOne after: ${countOne}`,
-      color: 'black',
-      bgColor: 'pink'
-    });
-
-    // setCountTwo(prevCount => prevCount + 1);
-  }, [countOne]);
-
-  useLayoutEffect(() => {
-    console({
-      value: `USE_LAYOUT_EFFECT count one: ${countOne}`,
-      color: 'black',
-      bgColor: 'red'
-    });
-  });
-
-  useUpdate(async () => {
-    console({
-      value: `USE_UPDATE count one: ${countOne}`,
-      color: 'black',
-      bgColor: 'lightgreen'
-    });
-
-    setCountTwo(prevCount => prevCount + 1);
-  }, [countOne]);
-
-  useUpdate(() => {
-    console({
-      value: `USE_UPDATE 2 count one: ${countOne}`,
-      color: 'black',
-      bgColor: 'yellow'
-    });
-  }, [countOne]);
-
-  useUpdate(() => {
-    console({
-      value: `USE_UPDATE count two: ${countTwo}`,
-      color: 'black',
-      bgColor: 'lightgrey'
-    });
-  }, [countTwo]);
-
-  console({
-    value: `RENDER count one: ${countOne}`,
-    color: 'black',
-    bgColor: 'orange'
-  });
+  const [prop, setProp] = useState({ a: 0 });
 
   return (
-    <div>
-      <button onClick={update} type="button">
-        Update
+    <>
+      <AwaitSetStateChild prop={prop} />
+      <button
+        onClick={() => {
+          setProp(prevProp => ({ ...prevProp, a: prevProp.a + 1 }));
+        }}
+      >
+        Update Prop
       </button>
-    </div>
+    </>
   );
 };
 
---
+++ src/pages/AwaitSetStateFuncCmp/AwaitSetStateChild.js
@@ -0,0 +1,99 @@
+import React, { memo, useCallback, useLayoutEffect, useState } from 'react';
+
+import { console, useUpdate, useUpdateOnly } from '../../services';
+
+let a = 20;
+
+const AwaitSetState = ({ prop }) => {
+  const [countOne, setCountOne] = useState(0);
+  const [countTwo, setCountTwo] = useState(0);
+
+  const updateCountOne = useCallback(async () => {
+    console({
+      value: `Func count ONE, a, prop, before: ${countOne}, ${a}, ${prop.a}`,
+      color: 'black',
+      bgColor: 'lightBlue'
+    });
+
+    // await setState (none documented behaviour).
+    await setCountOne(prevCount => prevCount + 1);
+
+    console({
+      value: `Func count ONE after: ${countOne}`,
+      color: 'white',
+      bgColor: 'blue'
+    });
+
+    // setCountTwo(prevCount => prevCount + 1);
+  }, [countOne]);
+
+  const updateCountTwo = useCallback(async () => {
+    setCountTwo(prevCount => prevCount + 1);
+  }, []);
+
+  // useLayoutEffect(() => {
+  //   console({
+  //     value: `USE_LAYOUT_EFFECT count ONE: ${countOne}`,
+  //     color: 'black',
+  //     bgColor: 'red'
+  //   });
+  // });
+
+  useUpdate(async () => {
+    console({
+      value: `USE_UPDATE 1 count ONE, count TWO, before: ${countOne}, ${countTwo}`,
+      color: 'black',
+      bgColor: 'lightgreen'
+    });
+
+    await setCountTwo(prevCount => prevCount + 1);
+
+    console({
+      value: `USE_UPDATE 1 count ONE, count TWO after: ${countOne}, ${countTwo}, ${a}`,
+      color: 'white',
+      bgColor: 'green'
+    });
+  }, [countOne]);
+
+  useUpdate(() => {
+    console({
+      value: `USE_UPDATE 2 count ONE: ${countOne}`,
+      color: 'black',
+      bgColor: 'yellow'
+    });
+  }, [countOne]);
+
+  useUpdate(() => {
+    a = a + 1;
+    console({
+      value: `USE_UPDATE 1 count TWO: ${countTwo}`,
+      color: 'black',
+      bgColor: 'lightgrey'
+    });
+  }, [countTwo]);
+
+  console({
+    value: `RENDER count ONE: ${countOne}`,
+    color: 'black',
+    bgColor: 'orange'
+  });
+
+  console({
+    value: `RENDER count TWO: ${countTwo}`,
+    color: 'black',
+    bgColor: 'orange'
+  });
+
+  return (
+    <div>
+      <button onClick={updateCountOne} type="button">
+        Update Count One
+      </button>
+      <button onClick={updateCountTwo} type="button">
+        Update Count Two
+      </button>
+    </div>
+  );
+};
+
+export default memo(AwaitSetState);
