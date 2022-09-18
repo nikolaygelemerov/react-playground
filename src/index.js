bcc233b5594e96cff4c316bad71ca2b60d750748
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2022-09-18 23:45:29 +0300

    Create `useUpdateOnly` hook

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import reportWebVitals from './reportWebVitals';
 import { Router } from './hoc';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
+root.render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/Mount/Mount.js
+++ src/pages/Mount/Mount.js
@@ -1,8 +1,84 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useRef, useState } from 'react';
+
+import { useUnmount, useIsMounted } from '@rounik/react-custom-hooks';
+
+const useMount = callback => {
+  const mountedRef = useRef(false);
+
+  useEffect(() => {
+    if (!mountedRef.current) {
+      callback();
+      mountedRef.current = true;
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export const useUpdateOnly = (callback, dependencyList = []) => {
+  const prevDependencyListRef = useRef(dependencyList);
+
+  useEffect(() => {
+    const hasChange = dependencyList.some(
+      (dep, depIndex) => dep !== prevDependencyListRef.current[depIndex]
+    );
+
+    if (!hasChange) {
+      return;
+    }
+
+    prevDependencyListRef.current = dependencyList;
+
+    const result = callback();
+
+    return () => {
+      (async () => {
+        const func = await result;
+
+        if (typeof func === 'function') {
+          func();
+        }
+      })();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, dependencyList);
+};
 
 const Mount = () => {
-  console.log('mount');
-  return <div>Mount</div>;
+  const [update, setUpdate] = useState({});
+
+  useMount(() => {
+    //   console.log('USE MOUNT');
+  });
+
+  useUpdateOnly(() => {
+    console.log('USE UPDATE ONLY');
+  }, [update]);
+
+  useEffect(() => {
+    // console.log('MOUNT EFFECT');
+
+    return () => {
+      // console.log('UNMOUNT EFFECT');
+    };
+  }, []);
+
+  useUnmount(() => {
+    // console.log('USE UNMOUNT');
+  });
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setUpdate({});
+        }}
+        type="button"
+      >
+        Update
+      </button>
+    </div>
+  );
 };
 
 export default memo(Mount);
